b247956ae3619dbb5c06bb34c21a63d737f5e068
fjordimm <fjordimm@gmail.com>
2023-06-05 14:17:34 -0700

    completed the readme, added comments, fixed an important bug

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # finding_twin_carmichael_nums
 
-Created for a friend's personal project
+## Purpose
+
+Finds all (up to a certain amount) pairs of non-prime Carmichael numbers that have a gap within a certain threshold.
+
+Created for a friend's personal project.
+
+## Usage
+
+Adjust SEARCH_N, SEARCH_GAP, and PRIMES_MEMO_SIZE in `carmichael_twin_search.c`
+
+## Example
+where SEARCH_N = 50000000, SEARCH_GAP = 1000, PRIMES_MEMO_SIZE = 8388608
+
+Output:
+
+```
+Searching for gap of 1000 or less
+
+561 and 1105 with gap = 544
+1105 and 1729 with gap = 624
+1729 and 2465 with gap = 736
+2465 and 2821 with gap = 356
+8719309 and 8719921 with gap = 612
+
+Done searching through n = 50000000
+```
--- carmichael_twin_search.c
+++ carmichael_twin_search.c
@@ -9,8 +9,8 @@ Last Updated: 6-5-2023
 Credits:
  - Uses almost direct copies of code from https://codegolf.stackexchange.com/a/974 for:
     - is_prime()
-	- prime()
-	- is_carmichael()
+    - prime()
+    - is_carmichael()
 
 Machine used by author:
 	Windows 11 (OS Build 22621.1702)
@@ -19,63 +19,60 @@ Compiled using:
 
 */
 
+///////
+
+// SEARCH_N: The number to stop searching at
+//  - Must be under the maximum value of the 'carmich_t' type
+//  - The number of prime numbers less than or equal to SEARCH_N must be smaller than PRIMES_MEMO_SIZE
+#define SEARCH_N 50000000
+
+// SEARCH_DIFF: The maximum gap of two Carmichael numbers for them to be printed out
+#define SEARCH_GAP 1000
+
+// PRIMES_MEMO_SIZE: The size of the prime number memoization array
+//  - Will break for large numbers depending on the machine (it is the length for a static array of 'carmich_t's)
+#define PRIMES_MEMO_SIZE 8388608 // arbitrary, but currently set to 2^23
+
+///////
+
 #include <stdio.h>
 #include <stdbool.h>
 
-#define intl unsigned long long int
+#define carmich_t unsigned long long int
 
-#define PRIMES_MEMO_SIZE 10000
-
-/*
-Requirements for SEARCH_N:
- - must be under the maximum value of intl
- - the number of prime numbers up to SEARCH_N must be less than PRIMES_MEMO_SIZE
-*/
-#define SEARCH_N 100000000
-
-#define SEARCH_DIFF 1000
-
-bool is_prime(intl v);
-intl prime(intl i, intl* i2);
-bool is_carmichael(intl n);
+bool is_prime(carmich_t v);
+carmich_t prime(carmich_t i, carmich_t* i2);
+bool is_carmichael(carmich_t n);
 
 int main(void)
 {
-	for (intl i = 1; i < 10000; i++)
+	carmich_t last_found = 0;
+
+	printf("Searching for gap of %llu or less\n\n", SEARCH_GAP);
+
+	for (carmich_t i = 1; i <= SEARCH_N; i++)
 	{
 		if (is_carmichael(i))
 		{
-			printf("%llu\n", i);
-		}
-	}
-
-/*
-	intl last_found = 0;
-
-	printf("Searching for difference of %llu or less\n\n", SEARCH_DIFF);
-
-	for (intl i = 1; i <= SEARCH_N; i++)
-	{
-		if (is_carmichael(i))
-		{
-			if (i - last_found <= SEARCH_DIFF)
+			if ((last_found != 0) && (i - last_found <= SEARCH_GAP))
 			{
-				printf("%llu and %llu with difference = %llu\n", last_found, i, i - last_found);
-				last_found = i;
+				printf("%llu and %llu with gap = %llu\n", last_found, i, i - last_found);
 			}
+			
+			last_found = i;
 		}
 	}
 
 	printf("\nDone searching through n = %llu\n", SEARCH_N);
-*/
+
 	return 0;
 }
 
-bool is_prime(intl v)
+bool is_prime(carmich_t v)
 {
 	bool isprm = true;
 	
-	for (intl i = 3, i2 = 9; i2 < v; i++, i2 += i + i + 1)
+	for (carmich_t i = 3, i2 = 9; i2 < v; i++, i2 += i + i + 1)
 	{
 		if (v % i == 0)
 		{
@@ -87,15 +84,15 @@ bool is_prime(intl v)
 	return isprm;
 }
 
-intl prime(intl i, intl* i2)
+carmich_t prime(carmich_t i, carmich_t* i2)
 {
-	static intl primes_memo[PRIMES_MEMO_SIZE] = {2, 3, 5};
-	static intl primes_sqrd_memo[PRIMES_MEMO_SIZE] = {4, 9, 25};
+	static carmich_t primes_memo[PRIMES_MEMO_SIZE] = {2, 3, 5};
+	static carmich_t primes_sqrd_memo[PRIMES_MEMO_SIZE] = {4, 9, 25};
 	static size_t primes_memo_cnt = 3;
 
 	while (i >= primes_memo_cnt)
 	{
-		intl v = primes_memo[primes_memo_cnt - 1];
+		carmich_t v = primes_memo[primes_memo_cnt - 1];
 
 		do
 		{ v += 2; }
@@ -110,16 +107,16 @@ intl prime(intl i, intl* i2)
 	return primes_memo[i];
 }
 
-bool is_carmichael(intl n)
+bool is_carmichael(carmich_t n)
 {
 	if (n < 2)
 	{ return false; }
 
-	intl k = n;
+	carmich_t k = n;
 	n--;
-	intl i2 = 4;
+	carmich_t i2 = 4;
 
-	for (intl i = 1, p = 2; i2 <= k; p = prime(i++, &i2))
+	for (carmich_t i = 1, p = 2; i2 <= k; p = prime(i++, &i2))
 	{
 		if (k % p == 0)
 		{
@@ -132,5 +129,6 @@ bool is_carmichael(intl n)
 		}
 	}
 
-	return (--k != n) && (n % k == 0);
+	k--;
+	return (k != n) && (n % k == 0);
 }
